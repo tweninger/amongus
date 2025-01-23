a011e03a23fdec7a46f69d7ce132d52f43b0915c
Satvik Golechha <7atvik@gmail.com>
2025-01-22 21:01:12 -0800

    engineering setup

--- AmongUs/constants.py
+++
@@ -1,6 +0,0 @@
-"""Constants for the AmongUs package."""
-
-PROJECT_NAME = "AmongUs"
-PROJECT_SHORT = "pt"
-WANDB_ENTITY = "farai"
-WANDB_PROJECT = "AmongUs"
--- AmongUs/tests/test_training.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-from AmongUs.training import NeuralNetwork
-
-
-def test_neural_network():
-    model = NeuralNetwork()
-    assert isinstance(model, torch.nn.Module)
