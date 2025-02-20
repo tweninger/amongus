a7b0943d131e5f72b53c7c096dc7ca73f6322529
Satvik Golechha <7atvik@gmail.com>
2025-02-20 08:25:07 +0000

    update cache activations'

--- linear-probes/cache_activations.py
+++ linear-probes/cache_activations.py
@@ -28,7 +28,7 @@ LOGS_PATH, RAW_PATH = "../evaluations/results/", "../expt-logs/"
 sys.path.append("..")
 EXPT_NAME, DESCRIPTIONS = "2025-02-01_phi_phi_100_games_v3", "Crew: Phi, Imp: Phi"
 
-dataset = AmongUsDataset({**config, "test_split": 1.0}, model=model, tokenizer=tokenizer, device=device, expt_name=EXPT_NAME)
+dataset = AmongUsDataset(config, model=model, tokenizer=tokenizer, device=device, expt_name=EXPT_NAME, test_split=1.0)
 eval(f"model.{config['hook_component']}").register_forward_hook(dataset.activation_cache.hook_fn)
 
 dataset.populate_dataset(force_redo=True, max_rows=0, batched=False, seq_len=config["seq_len"], num_tokens=None)
