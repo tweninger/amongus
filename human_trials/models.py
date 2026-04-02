e994c4157b4910b827220826bd5f33ffe8dfc8d7
Michael Bai <michaelbai30@gmail.com>
2026-04-02 12:43:13 -0400

    Added models.py

---
+++ human_trials/models.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import asyncio
+import networkx as nx
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+research_path = os.path.abspath(os.path.join(current_dir, "..", "among-agents"))
+if research_path not in sys.path:
+    sys.path.append(research_path)
+
+from amongagents.envs.configs.map_config import room_data, connections, vent_connections
+
+class Map:
+    """Handles the physical layout of the ship and connectivity."""
+    def __init__(self):
+        self.ship_map = nx.Graph()
+        
+        for room_name, details in room_data.items():
+            self.ship_map.add_node(room_name, **details)
+
+        for room1, room2 in connections:
+            self.ship_map.add_edge(room1, room2, connection_type="corridor")
+
+        for room1, room2 in vent_connections:
+            self.ship_map.add_edge(room1, room2, connection_type="vent")
+
+    def get_adjacent_rooms(self, room_name):
+        if room_name not in self.ship_map:
+            return []
+        return [
+            adj for adj, attr in self.ship_map[room_name].items()
+            if attr["connection_type"] == "corridor"
+        ]
+
+# Map is a singleton
+skeld = Map()
+
+class WebPlayerAgent:
+    """The Human-to-Engine Bridge. Holds the 'brain' of the player."""
+    def __init__(self, player):
+        self.player = player
+        self.model = "homosapiens/brain1.0"
+        self.queued_action = None
+
+    async def choose_action(self, timestep):
+        # Blocking loop
+        # Pause until human has returned a response via API
+        while self.queued_action is None:
+            await asyncio.sleep(0.5)
+        action = self.queued_action
+        self.queued_action = None
+        return action
+
+    def choose_observation_location(self, map):
+        return self.player.location
