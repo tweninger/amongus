9556fe74d06df70afe8e3cbcb23cc572fc67a819
Michael Bai <michaelbai30@gmail.com>
2026-04-06 12:56:30 -0400

    Added config.js to store some helpers

---
+++ human_trials/static/js/config.js
@@ -0,0 +1,21 @@
+// config.js
+
+export const roomCoordinates = {
+    "cafeteria": { top: 30, left: 50 },
+    "weapons": { top: 25, left: 75 },
+    "navigation": { top: 45, left: 88 },
+    "o2": { top: 42, left: 68 },
+    "shields": { top: 75, left: 75 },
+    "communication": { top: 85, left: 65 },
+    "communications": { top: 85, left: 65 },
+    "admin": { top: 60, left: 65 },
+    "storage": { top: 75, left: 50 },
+    "electrical": { top: 60, left: 35 },
+    "lower engine": { top: 75, left: 20 },
+    "reactor": { top: 50, left: 10 },
+    "security": { top: 50, left: 28 },
+    "upper engine": { top: 25, left: 20 },
+    "medbay": { top: 35, left: 35 }
+};
+
+export const PLAYER_COLORS = ["red", "blue", "green", "pink", "orange", "yellow", "black", "white", "purple", "brown", "cyan", "lime"];
