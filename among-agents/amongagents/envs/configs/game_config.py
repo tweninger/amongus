1dd8ff5fe8d50afff0c2c4f7333abd8c0b48f428
Michael Bai <michaelbai30@gmail.com>
2026-05-26 11:34:11 -0400

    Game balance changes

--- among-agents/amongagents/envs/configs/game_config.py
+++ among-agents/amongagents/envs/configs/game_config.py
@@ -13,9 +13,9 @@ THREE_MEMBER_GAME = {
 FIVE_MEMBER_GAME = {
     "num_players": 5,
     "num_impostors": 1,
-    "num_common_tasks": 1,
-    "num_short_tasks": 1,
-    "num_long_tasks": 0,
+    "num_common_tasks": 2,
+    "num_short_tasks": 2,
+    "num_long_tasks": 1,
     "discussion_rounds": 3,
     "max_num_buttons": 2,
     "kill_cooldown": 3,
